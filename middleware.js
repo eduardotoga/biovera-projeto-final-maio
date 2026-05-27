9011a14e6be50c900725d6b1bcfbdae6821b3070
eduardotoga <amscompanyamz@gmail.com>
2026-05-27 19:05:37 -0300

    Enhance middleware to block additional IP addresses

--- middleware.js
+++ middleware.js
@@ -1,15 +1,15 @@
 export const config = { matcher: '/' };
 
-export default function middleware(request) {
-  const ip = (request.headers.get('x-forwarded-for') || '').split(',')[0].trim();
+  export default function middleware(request) {
+    const ip = (request.headers.get('x-forwarded-for') || '').split(',')[0].trim();
 
-  if (ip === '168.232.42.103') {
-    return;
+    if (ip === '168.232.42.103' || ip === '177.75.104.45') {
+      return;
+    }
+
+    const city = decodeURIComponent(request.headers.get('x-vercel-ip-city') || '');
+
+    if (/chapec/i.test(city)) {
+      return new Response('Not Found', { status: 404 });
+    }
   }
-
-  const city = decodeURIComponent(request.headers.get('x-vercel-ip-city') || '');
-
-  if (/chapec/i.test(city)) {
-    return new Response('Not Found', { status: 404 });
-  }
-}
