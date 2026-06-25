513400a03ee47e26c5e4ce711ce9da699a8e69b1
eduardotoga <amscompanyamz@gmail.com>
2026-06-25 19:32:14 -0300

    Delete middleware.js and its functionality
    Remove middleware function and related config.

--- middleware.js
+++ middleware.js
@@ -1,15 +1,1 @@
-export const config = { matcher: '/' };
 
-  export default function middleware(request) {
-    const ip = (request.headers.get('x-forwarded-for') || '').split(',')[0].trim();
-
-    if (ip === '168.232.42.79' || ip === '177.75.104.45') {
-      return;
-    }
-
-    const city = decodeURIComponent(request.headers.get('x-vercel-ip-city') || '');
-
-    if (/chapec/i.test(city)) {
-      return new Response('Not Found', { status: 404 });
-    }
-  }
